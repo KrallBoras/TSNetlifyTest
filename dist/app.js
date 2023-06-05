4d19a314152d61abd09fe53a08100524652111a5
Jonathan Krall <jonathan.krall@lexicon.se>
2023-06-05 09:39:13 +0200

    initial

---
+++ dist/app.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(name) {
+    console.log(`Hello, ${name}`);
+}
+function test() {
+    console.log("This just a test");
+}
+function calculate(a, b) {
+    let intA = a;
+    let intB = b;
+    console.log(`${intA + intB}`);
+}
+greet("Krall");
+test();
---
+++ src/app.ts
@@ -0,0 +1,20 @@
+function greet(name: string){
+    console.log(`Hello, ${name}`);
+}
+
+function test()
+{
+    console.log("This just a test");
+}
+
+function calculate(a, b)
+{
+    let intA: number = a;
+    let intB: number = b;
+    console.log(`${intA + intB}`)
+}
+
+greet("Krall");
+test();
+
+export {};
